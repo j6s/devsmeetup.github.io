ce6aaf0304f915547741102b496f8019c5464623
Sebastian Sebald <sebastian.sebald@gmail.com>
2018-05-25 00:34:15 +0200

    🐛 Fix build script

--- build.js
+++ build.js
@@ -1,14 +1,45 @@
 const del = require('del');
-const { outputFile } = require('fs-extra');
+const express = require('express');
+const { copy, outputFile, readFile } = require('fs-extra');
+const globby = require('globby');
+const http = require('http');
 const Bundler = require('parcel-bundler');
 const { resolve } = require('path');
 const puppeteer = require('puppeteer');
-const serve = require('serve');
 
 const { main } = require('./package.json');
 
+// Config
+// ---------------
+const file = resolve(__dirname, main);
+const staticFiles = ['CNAME', 'manifest.json', 'robots.txt'];
+const out = resolve(__dirname, './dist');
+const port = 1337;
+
 // Helper
 // ---------------
+const serve = ({ folder, port }) =>
+  new Promise((resolve, reject) => {
+    const app = express();
+    app.use(express.static(folder));
+
+    const server = http.createServer(app);
+    const close = () =>
+      new Promise(resolve => {
+        server.close(() => {
+          console.log('Stopping server ...');
+          resolve();
+        });
+      });
+
+    server
+      .listen(port, () => {
+        console.log(`Serving files in "${folder}" ...`);
+        resolve(close);
+      })
+      .on('error', err => reject(err));
+  });
+
 const ssr = async url => {
   const browser = await puppeteer.launch();
   const page = await browser.newPage();
@@ -28,11 +59,19 @@ const ssr = async url => {
 
 const sanitize = html => html.replace(/<script.*<\/script>/g, '');
 
-// Config
-// ---------------
-const file = resolve(__dirname, main);
-const out = resolve(__dirname, './dist');
-const port = 1337;
+const fixManifest = async folder => {
+  const files = await globby([`${folder}/*.html`]);
+  const jsToJson = html => html.replace(/(href="\/manifest)(.*js)/g, '$1.json');
+
+  return Promise.all(
+    files.map(name =>
+      readFile(name, 'utf8').then(html => outputFile(name, jsToJson(html)))
+    )
+  );
+};
+
+const copyAll = ({ from, to, files }) =>
+  Promise.all(files.map(name => copy(`${from}/${name}`, `${to}/${name}`)));
 
 // Script
 // ---------------
@@ -50,16 +89,24 @@ void (async function() {
   const bundle = await bundler.bundle();
 
   // Serve build files
-  const server = serve(out, {
+  const close = await serve({
+    folder: out,
     port,
-    silent: true,
   });
 
   // Run puppeteer
-  let html = await ssr(`http://localhost:${port}`);
+  const html = await ssr(`http://localhost:${port}`);
 
-  // Overwrite previously created file (from bundle) + remove <script> tags
+  /**
+   * (1) Remove <script> tags, since they're no longer needed.
+   * (2) Rename manifest, parcel transform this to a JS file ...
+   */
   await outputFile(bundle.name, sanitize(html));
+  await fixManifest(out);
 
-  server.stop();
+  // Copy CNAME and other files
+  const { rootDir } = bundle.entryAsset.options;
+  await copyAll({ from: rootDir, to: out, files: staticFiles });
+
+  close();
 })();
